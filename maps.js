9ac7106e2b1e79af2abc71636e29307c2f2d6b1d
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2026-01-21 22:38:17 -0300

    promisse example

--- maps.js
+++ maps.js
@@ -14,3 +14,29 @@ class Car {
       this.year = year;
     }
   }
+
+const getUsers = new Promise((resolve, reject) => {
+    const success = true;
+  
+    setTimeout(() => {
+      if (success) {
+        resolve({ id: 1, nome: "Luiz" });
+      } else {
+        reject("Error");
+      }
+    }, 2000);
+  });
+
+
+getUsers
+  .then((user) => {
+    console.log("user found:", usuario);
+  })
+  .catch((error) => {
+    console.error("ERROR:", erro);
+  })
+  .finally(() => {
+    console.log("finish");
+  });
+
+  
