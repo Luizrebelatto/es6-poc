3949b2ab1bb58de09aabfce4fda788f44cb72570
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2026-01-22 08:25:21 -0300

    map set

---
+++ map-set.js
@@ -0,0 +1,15 @@
+// unique values
+const techs = new Set();
+
+techs.add("threeJS");
+techs.add("vitest");
+techs.add("fortuneSheet"); // Set(3) { 'threeJS', 'vitest', 'fortuneSheet' }
+
+
+// key-value
+const sellers = new Map([
+    ["luiz", 200],
+    ["patolino", 300],
+    ["frajola", 200]
+]);
+// Map(3) { 'luiz' => 200, 'patolino' => 300, 'frajola' => 200 }
